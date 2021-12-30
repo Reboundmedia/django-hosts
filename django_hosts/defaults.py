e7dbdf189cd11fd5c1ccbcc35aff6ca91061b4d7
Sherif Adigun <30326604+adigunsherif@users.noreply.github.com>
2021-12-30 13:28:00 +0100

    Update defaults.py

--- django_hosts/defaults.py
+++ django_hosts/defaults.py
@@ -93,7 +93,9 @@ class host(object):
         pattern to avoid rather unwieldy escaping in every definition.
         """
         self.regex = regex
-        self.compiled_regex = re.compile(r'%s(\.|$)' % regex)
+        parent_host = getattr(settings, 'PARENT_HOST', '').lstrip('.')
+        suffix = r'\.' + parent_host if parent_host else ''
+        self.compiled_regex = re.compile(r'%s%s(\.|:|$)' % (regex, suffix))
         self.urlconf = urlconf
         self.name = name
         self._scheme = scheme
